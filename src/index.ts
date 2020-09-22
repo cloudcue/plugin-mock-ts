9826ece5e05e97c458dba6c743f54b648fddf007
Nick <nlaurie@metafig.com>
2020-09-22 14:52:02 -0400

    Rev

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MockPlugin } from './plugin';
 import * as c1 from '@cloudcue/contracts';
 
+export const info: c1.PluginInfo = new MockPlugin();
+
 export function createPlugin(): c1.Plugin {
   return new MockPlugin();
 }
